a4dc8d6409badb8c880aa4090f9960a7480344cd
Isa <94014429+isa-nafe@users.noreply.github.com>
2023-06-14 09:10:26 +0000

    add access to static resources

--- src/main/java/com/asn2/asn2/config/WebConfiguration.java
+++ src/main/java/com/asn2/asn2/config/WebConfiguration.java
@@ -5,6 +5,7 @@ package com.asn2.asn2.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,6 +14,13 @@ public class WebConfiguration implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/students/**").allowedMethods("*");
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry
+        .addResourceHandler("/static/**")
+        .addResourceLocations("classpath:/static/");
   }
 }
 
